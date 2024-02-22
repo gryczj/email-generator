a988ed5ba5457e44ac33b6548ec87a9110d6064f
Joanna Grycz <jgrycz@concisesoftware.com>
2024-02-22 16:57:12 +0100

    Add register and login modules, kysely and database integration

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { UsersRepository } from './users.repository';
+import { LoginDTO, RegistrationDTO } from './dtos';
+import { LoginResponse } from './login.response';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async register({
+    username,
+    password,
+    openAIKey,
+  }: RegistrationDTO): Promise<any> {
+    return this.usersRepository.getAll();
+  }
+
+  async login({ username, password }: LoginDTO): Promise<LoginResponse> {
+    return {} as LoginResponse;
+  }
+}
---
+++ src/auth/login.response.ts
@@ -0,0 +1,4 @@
+export interface LoginResponse {
+  authToken: string;
+  expiresIn: number;
+}
